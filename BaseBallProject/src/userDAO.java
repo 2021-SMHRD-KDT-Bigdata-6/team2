b12140d05f88510f4060b42f3c9a937c09435bec
smhrd <smhrd@211.223.106.104>
2021-09-29 12:22:38 +0900

    test

--- BaseBallProject/src/userDAO.java
+++ BaseBallProject/src/userDAO.java
@@ -1,12 +1,51 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class userDAO {
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	private Connection conn;
+
+	private void getConn() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String db_url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String db_id = "hr";
+			String db_pw = "hr";
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void close() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// 로그인
+	public userVO login(userVO vo) {
+		userVO info = null;
+		getConn();
+
+		return info;
+	}
+
+	// 회원가입
+	public int signup(userVO vo) {
+		int cnt = 0;
+
+		getConn();
+
+		return cnt;
+	}
+
 }

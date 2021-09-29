e100eecc3deac6c2ab0b95e51ea2eac1593bc975
smhrd <smhrd@119.206.166.38>
2021-09-29 12:31:57 +0900

    PlayerDAO, VO생성

---
+++ BaseBallProject/src/PlayerDAO.java
@@ -0,0 +1,78 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PlayerDAO {
+
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	private void getConn() {
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String db_url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String db_id = "cgi_5_2";
+			String db_pw = "smhrd2";
+			conn = DriverManager.getConnection(db_url, db_id, db_pw);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private int playerInput(PlayerVO vo) {
+		
+		int num = 0;
+		String name = "";
+		int stat = 0;
+		
+		PlayerVO player = new PlayerVO(num,name,stat);
+		
+		int cnt = 0;
+
+		getConn();
+
+		try {
+			String sql = "update players set name = ? Where employee_id = ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, vo.getNum());
+			psmt.setString(2, vo.getName());
+			psmt.setInt(3, vo.getStat());
+			cnt = psmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return cnt;
+
+	}
+
+}
---
+++ BaseBallProject/src/PlayerVO.java
@@ -0,0 +1,53 @@
+
+public class PlayerVO {
+	
+	private String name;
+	private int num;
+	private int stat;
+	
+	
+	public PlayerVO(int num, String name, int stat) {
+		this.name = name;
+		this.num = num;
+		this.stat = stat;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getNum() {
+		return num;
+	}
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+	public int getStat() {
+		return stat;
+	}
+
+
+	public void setStat(int stat) {
+		this.stat = stat;
+	}
+	
+	public String toString() {
+		return "";
+	}
+	
+	
+	
+	
+
+}

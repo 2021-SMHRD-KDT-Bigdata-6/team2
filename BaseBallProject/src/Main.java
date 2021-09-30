be2ac6c4a88a6a2f00e3a53174b1af0394afc3d1
smhrd <smhrd@211.223.106.104>
2021-09-30 16:01:51 +0900

    로그인실패시 경기화면 수정

--- BaseBallProject/src/Main.java
+++ BaseBallProject/src/Main.java
@@ -19,56 +19,59 @@ public class Main {
 				// login 구현하기
 
 				// login
-				System.out.println("--로그인--");
-				System.out.print("ID입력 : ");
-				String id = sc.next();
-				System.out.print("PW입력 : ");
-				String pw = sc.next();
-				
-				userVO vo = new userVO(id, pw);
-				userVO info = dao.login(vo);
-				
-				if(info != null) {
-					System.out.println("로그인 성공!");
-					System.out.println(info.getId() + "님 환영합니다!");
-				}else {
-					System.out.println("로그인 실패..");
-				}
-				
-				while (true) {
-					System.out.print("[1]경기시작  [2]랭킹보기  [3]이전화면");
-					int select2 = sc.nextInt();
-					if (select2 == 1) { // 경기시작
-						daoP.game(vo.getId(), daoP.enemyPick(id));
+				int login = 0;
+				while (login == 0) {
+					System.out.println("--로그인--");
+					System.out.print("ID입력 : ");
+					String id = sc.next();
+					System.out.print("PW입력 : ");
+					String pw = sc.next();
 
-						
-						
-						// 우리팀, 상대방 팀 불러오기
-						// String enemyId = daoP.enemyPick(id);
-						// int myPlayer;
-						// int enemyPlayer;
-						// System.out.println("내 출전선수 번호 "+ (myPlayer = daoP.playerPick(id)));
-						// System.out.println("상대 출전선수 번호 "+ (enemyPlayer = daoP.playerPick(enemyId)));
-						
-						// show_playerList
-						
-						// 승리시, playerInput(선수등록)
-						
-						// 승리시, ranking
-					} else if (select2 == 2) {
-						// ranking
-						System.out.println("***BaseBall 랭킹!***");
-						ArrayList<userVO> ranList = dao.ranking();
-						for (int i = 0; i < ranList.size(); i++) {
-							System.out.print(i + 1 + "위 ");
-							System.out.print(ranList.get(i));
-							System.out.println();
+					userVO vo = new userVO(id, pw);
+					userVO info = dao.login(vo);
+
+					if (info != null) {
+						System.out.println("로그인 성공!");
+						System.out.println(info.getId() + "님 환영합니다!");
+						login++;
+
+						while (true) {
+							System.out.print("[1]경기시작  [2]랭킹보기  [3]이전화면");
+							int select2 = sc.nextInt();
+							if (select2 == 1) { // 경기시작
+								daoP.game(vo.getId(), daoP.enemyPick(id));
+
+								// 우리팀, 상대방 팀 불러오기
+								// String enemyId = daoP.enemyPick(id);
+								// int myPlayer;
+								// int enemyPlayer;
+								// System.out.println("내 출전선수 번호 "+ (myPlayer = daoP.playerPick(id)));
+								// System.out.println("상대 출전선수 번호 "+ (enemyPlayer = daoP.playerPick(enemyId)));
+
+								// show_playerList
+
+								// 승리시, playerInput(선수등록)
+
+								// 승리시, ranking
+							} else if (select2 == 2) {
+								// ranking
+								System.out.println("***BaseBall 랭킹!***");
+								ArrayList<userVO> ranList = dao.ranking();
+								for (int i = 0; i < ranList.size(); i++) {
+									System.out.print(i + 1 + "위 ");
+									System.out.print(ranList.get(i));
+									System.out.println();
+								}
+							} else if (select2 == 3) {
+								System.out.println("메인으로 돌아갑니다.");
+								break;
+							} else {
+								System.out.println("1~3번 중에 입력해주세요!!");
+							}
 						}
-					} else if (select2 == 3) {
-						System.out.println("메인으로 돌아갑니다.");
-						break;
+
 					} else {
-						System.out.println("1~3번 중에 입력해주세요!!");
+						System.out.println("로그인 실패..");
 					}
 				}
 
@@ -101,12 +104,12 @@ public class Main {
 						}
 						System.out.println();
 						daoP.showPlayerList(id);
-					} //else {
+					} // else {
 //						System.out.println("회원가입 실패...");
 //					}
 				}
 				// show_playerList
-				
+
 			} else if (select == 3) {
 				System.out.print("진짜 종료하시겠습니까?ㅜ >> (y/n)");
 				String answer = sc.next();
